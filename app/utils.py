0d81eed74b0bf594b4b08cddd6fa0374e456ab75
prueba <prueba@gmail.com>
2023-07-19 16:00:06 -0400

    add docs

---
+++ app/utils.py
@@ -0,0 +1,46 @@
+
+def get_population(country_dict):
+  population_dict = {
+    '2022': int(country_dict['2022 Population']),
+    '2020': int(country_dict['2020 Population']),
+    '2015': int(country_dict['2015 Population']),
+    '2010': int(country_dict['2010 Population']),
+    '2000': int(country_dict['2000 Population']),
+    '1990': int(country_dict['1990 Population']),
+    '1980': int(country_dict['1980 Population']),
+    '1970': int(country_dict['1970 Population'])
+  }
+
+  labels = population_dict.keys()
+  values = population_dict.values()
+  return labels, values
+
+
+def population_by_country(data, country):
+
+  result = list(filter(lambda item: item['Country'] == country, data))
+  return result
+
+'''
+import utils
+
+keys,values = utils.get_population()
+print(keys,values) # ['col','bol'] [300,400]
+
+data = [{
+	'Country': 'Colombia',
+	'Population': 500
+},
+{
+	'Country': 'Bolivia',
+	'Population': 300
+}
+]
+
+country = input('Type Country => ')
+
+result = utils.population_by_country(data, country)
+print(result)  # [{'Country': 'Colombia','Population': 500}]
+
+print(utils.A)
+'''
